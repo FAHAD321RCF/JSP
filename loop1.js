4f942079a51844c5cc97a7ec653894f0f22aea84
FAHAD321RCF <rcffahad1018@gmail.com>
2025-06-25 22:24:42 +0600

    add neeww code

---
+++ loop1.js
@@ -0,0 +1,12 @@
+// const friends = ['fahad', "rishhad", "rcf" , 'chy'];
+
+// for ( const friend of friends){
+
+//     console.log(friend);
+// }
+
+
+const number = [9, 1,2,3,4,5,5,6,7];
+
+const num = number.sort();
+console.log(num);
