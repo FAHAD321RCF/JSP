7ddb10f585d6844dd0e4e53011c50b42ac37eead
FAHAD321RCF <rcffahad1018@gmail.com>
2025-06-25 17:44:34 +0600

    add new coddee

---
+++ obj.js
@@ -0,0 +1,17 @@
+// const car ={
+//     brand :'bmw',
+//     color : "white",
+//     price : "one crore"
+
+// }
+// console.log(car.brand);
+
+const car ={
+    brand :'bmw',
+    color : "white",
+    price : "one crore"
+
+}
+const keys = Object.keys(car)
+
+console.log(keys)
